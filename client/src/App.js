b07af457f2992a646db6355e94cdfa4a609d5a44
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2020-03-12 02:39:41 -0700

    Adding api to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from "react";
+import ReactMapGL from "react-map-gl";
+
+import { listLogEntries } from "./Api";
 
 const App = () => {
-  const [viewport, setViewport] = useState({
-    width: '100vw',
-    height: '100vh',
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 3
-  });
+ const [viewport, setViewport] = useState({
+  width: "100vw",
+  height: "100vh",
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 3
+ });
 
-  return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={setViewport}
-      mapStyle="mapbox://styles/sbrueck/ck5u5v1bt184i1il6mpii6l4r"
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-    />
-  );
-}
+ useEffect(() => {
+   (async () => {
+     const logEntries = await listLogEntries();
+     console.log(logEntries);
+   })();
+ }, []);
+
+ return (
+  <ReactMapGL
+   {...viewport}
+   onViewportChange={setViewport}
+   mapStyle="mapbox://styles/sbrueck/ck5u5v1bt184i1il6mpii6l4r"
+   mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+  />
+ );
+};
 
 export default App;
